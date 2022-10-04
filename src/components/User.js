897684144561ffbc4360d7c719e90cc330ff4c6e
Michael Campbell <michael.campbell14@gmail.com>
2022-10-04 16:59:30 -0400

    using class based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,18 @@
+import React, { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// using extends Component on this class allows us to use and pass in props
+class User extends Component {
+  render() {
+    // we are only able to use "this."" is because of the extends Component
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// Functional Component
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 import User from './User';
 
 import classes from './Users.module.css';
